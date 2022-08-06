4fb0bd796e3941bc2925875097590de92f548c45
parsaeissazadeh <parsa.eissazadeh@snapp.cab>
2022-08-06 20:47:25 +0430

    define csvOpener class and its factory method

--- internal/csv/csvOpener.go
+++ internal/csv/csvOpener.go
@@ -1,7 +1,39 @@
 package csv
 
+import (
+	"log"
+	"os"
+)
+
 type Address struct {
 	Firstname string `csv:"firstname"`
 	Lastname  string `csv:"lastname"`
 	Address   string `csv:"address"`
+}
+
+type csvOpener struct {
+	path string
+}
+
+type CsvOpener interface {
+	open()
+}
+
+func NewCsvopener(path string) CsvOpener {
+	return &csvOpener{
+		path: path,
+	}
+}
+
+func (receiver *csvOpener) open() {
+	csvFile, err := os.Open("../addresses.csv")
+	if err != nil {
+		log.Fatalf("csv file could not be opened %v", err)
+	}
+	defer csvFile.Close()
+
+	// this csvLines is a 2D array of strings
+	for _, line := range csvLines {
+
+	}
 }
